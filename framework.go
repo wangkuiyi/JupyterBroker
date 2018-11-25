5cd59800952466096c9fed0de27fb771abc38268
Yi Wang <yi.wang.2005@gmail.com>
2018-11-25 14:17:01 -0800

    Restructure code (#5)
    * Shorten the code
    
    * Shorten the code
    
    * Restructure code

--- framework.go
+++ framework.go
@@ -1,27 +1,69 @@
 package jupyterbroker
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"os/exec"
 )
 
-type ProcessBroker struct {
+// Runner defines an action which writes output to io.Writer.
+type Runner interface {
+	Run(w io.Writer)
 }
 
-func setServerSentEventHeader(rw http.ResponseWriter) {
-	rw.Header().Set("Content-Type", "text/event-stream")
-	rw.Header().Set("Cache-Control", "no-cache")
-	rw.Header().Set("Connection", "keep-alive")
-	rw.Header().Set("Access-Control-Allow-Origin", "*")
+// ProcessRunner is an example implementation of Runner.
+type ProcessRunner struct {
+	cmd        string
+	args, envs []string
 }
 
-func (br *ProcessBroker) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	setServerSentEventHeader(rw)
+// Run a command in a sub-process, writing stdout and stderr to w.
+// With whatever error, just panic it.
+func (pr *ProcessRunner) Run(w io.Writer) {
+	cmd := exec.Command(pr.cmd, pr.args...)
+	cmd.Env = append(os.Environ(), pr.envs...)
+	cmd.Stdout = w
+	cmd.Stderr = w
+	if e := cmd.Run(); e != nil {
+		log.Panicf("ProcessRunner.Run: %v", e)
+	}
+}
 
+// ProcessRunnerHandler is an example broker handler, which creates a
+// ProcessRunner by parsing the URL and runs the runner.
+func ProcessRunnerHandler(rw http.ResponseWriter, req *http.Request) {
 	req.ParseForm()
-	pr := NewProcessRunner(req.Form["cmd"][0], req.Form["args"], req.Form["envs"])
+	pr := ProcessRunner{
+		cmd:  req.Form["cmd"][0], // MakeSSEHandler will guard panics.
+		args: req.Form["args"],
+		envs: req.Form["envs"]}
 	pr.Run(rw)
+}
+
+// SetServerSentEventHeader marks an http.ResponseWriter of SSEs.
+func SetServerSentEventHeader(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+// MakeSSEHandler returns a handler that guards panics in the given handler.
+func MakeSSEHandler(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if e := recover(); e != nil {
+				http.Error(w, fmt.Sprintf("%v", e),
+					http.StatusInternalServerError)
+			}
+		}()
+		SetServerSentEventHeader(w)
+		f(w, r)
+	}
 }
 
 func Start(addr string) (string, error) {
--- framework_test.go
+++ framework_test.go
@@ -2,24 +2,48 @@ package jupyterbroker
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFrameworkRun(t *testing.T) {
-	http.Handle("/myproc", &ProcessBroker{})
+func ExampleProcessBroker() {
+	http.HandleFunc("/process", MakeSSEHandler(ProcessRunnerHandler))
+	addr, _ := Start(":0")
+
+	res, _ := http.Get(fmt.Sprintf(
+		"http://%s/process?cmd=echo&args=hello&args=world", addr))
+	defer res.Body.Close()
+	io.Copy(os.Stdout, res.Body)
+	// Output:
+	// hello world
+}
+
+func TestProcessBroker(t *testing.T) {
+	// Start the server.
+	http.HandleFunc("/myproc", MakeSSEHandler(ProcessRunnerHandler))
 	addr, e := Start(":0")
 	assert.NoError(t, e)
 
+	// Ask the server to run bash.
 	res, e := http.Get(fmt.Sprintf(
-		"http://%s/myproc?cmd=echo&args=hello&args=world", addr))
+		"http://%s/myproc?cmd=sh&args=%%2Dc&args=echo%%20hello%%20%%24foo&envs=foo%%3dbar", addr))
 	assert.NoError(t, e)
-
 	txt, e := ioutil.ReadAll(res.Body)
 	assert.NoError(t, e)
-	assert.Equal(t, "hello world\n", string(txt))
+	assert.Equal(t, "hello bar\n", string(txt))
+	res.Body.Close()
+
+	// Ask the server to run nothing.
+	res, e = http.Get(fmt.Sprintf("http://%s/myproc", addr))
+	assert.NoError(t, e) // NOTE: http.Error doesn't signal the error code.
+	txt, e = ioutil.ReadAll(res.Body)
+	assert.NoError(t, e)
+	assert.True(t, strings.HasPrefix(string(txt), "runtime error:"))
 	res.Body.Close()
 }
