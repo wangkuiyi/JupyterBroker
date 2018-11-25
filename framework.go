7c8e8dea3d92c681561705952b3f10375e93fb56
Yi Wang <yi.wang.2005@gmail.com>
2018-11-25 14:14:59 -0800

    Restructure code

--- framework.go
+++ framework.go
@@ -6,19 +6,53 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"os/exec"
 )
 
+// Runner defines an action which writes output to io.Writer.
 type Runner interface {
 	Run(w io.Writer)
 }
 
-func setServerSentEventHeader(w http.ResponseWriter) {
+// ProcessRunner is an example implementation of Runner.
+type ProcessRunner struct {
+	cmd        string
+	args, envs []string
+}
+
+// Run a command in a sub-process, writing stdout and stderr to w.
+// With whatever error, just panic it.
+func (pr *ProcessRunner) Run(w io.Writer) {
+	cmd := exec.Command(pr.cmd, pr.args...)
+	cmd.Env = append(os.Environ(), pr.envs...)
+	cmd.Stdout = w
+	cmd.Stderr = w
+	if e := cmd.Run(); e != nil {
+		log.Panicf("ProcessRunner.Run: %v", e)
+	}
+}
+
+// ProcessRunnerHandler is an example broker handler, which creates a
+// ProcessRunner by parsing the URL and runs the runner.
+func ProcessRunnerHandler(rw http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
+	pr := ProcessRunner{
+		cmd:  req.Form["cmd"][0], // MakeSSEHandler will guard panics.
+		args: req.Form["args"],
+		envs: req.Form["envs"]}
+	pr.Run(rw)
+}
+
+// SetServerSentEventHeader marks an http.ResponseWriter of SSEs.
+func SetServerSentEventHeader(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 }
 
+// MakeSSEHandler returns a handler that guards panics in the given handler.
 func MakeSSEHandler(f http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
@@ -27,7 +61,7 @@ func MakeSSEHandler(f http.HandlerFunc) http.HandlerFunc {
 					http.StatusInternalServerError)
 			}
 		}()
-		setServerSentEventHeader(w)
+		SetServerSentEventHeader(w)
 		f(w, r)
 	}
 }
--- framework_test.go
+++ framework_test.go
@@ -4,60 +4,37 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
-	"os/exec"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-type ProcessRunner struct {
-	cmd        string
-	args, envs []string
-}
+func ExampleProcessBroker() {
+	http.HandleFunc("/process", MakeSSEHandler(ProcessRunnerHandler))
+	addr, _ := Start(":0")
 
-func (pr *ProcessRunner) Run(w io.Writer) {
-	cmd := exec.Command(pr.cmd, pr.args...)
-	cmd.Env = append(os.Environ(), pr.envs...)
-	cmd.Stdout = w
-	cmd.Stderr = w
-	if e := cmd.Run(); e != nil {
-		log.Panicf("ProcessRunner.Run: %v", e)
-	}
-}
-
-func processRunnerHandler(rw http.ResponseWriter, req *http.Request) {
-	req.ParseForm()
-	pr := ProcessRunner{
-		cmd:  req.Form["cmd"][0], // MakeSSEHandler will guard panics.
-		args: req.Form["args"],
-		envs: req.Form["envs"]}
-	pr.Run(rw)
+	res, _ := http.Get(fmt.Sprintf(
+		"http://%s/process?cmd=echo&args=hello&args=world", addr))
+	defer res.Body.Close()
+	io.Copy(os.Stdout, res.Body)
+	// Output:
+	// hello world
 }
 
 func TestProcessBroker(t *testing.T) {
 	// Start the server.
-	http.HandleFunc("/myproc", MakeSSEHandler(processRunnerHandler))
+	http.HandleFunc("/myproc", MakeSSEHandler(ProcessRunnerHandler))
 	addr, e := Start(":0")
 	assert.NoError(t, e)
 
-	// Ask the server to run echo.
-	res, e := http.Get(fmt.Sprintf(
-		"http://%s/myproc?cmd=echo&args=hello&args=world", addr))
-	assert.NoError(t, e)
-	txt, e := ioutil.ReadAll(res.Body)
-	assert.NoError(t, e)
-	assert.Equal(t, "hello world\n", string(txt))
-	res.Body.Close()
-
 	// Ask the server to run bash.
-	res, e = http.Get(fmt.Sprintf(
+	res, e := http.Get(fmt.Sprintf(
 		"http://%s/myproc?cmd=sh&args=%%2Dc&args=echo%%20hello%%20%%24foo&envs=foo%%3dbar", addr))
 	assert.NoError(t, e)
-	txt, e = ioutil.ReadAll(res.Body)
+	txt, e := ioutil.ReadAll(res.Body)
 	assert.NoError(t, e)
 	assert.Equal(t, "hello bar\n", string(txt))
 	res.Body.Close()
